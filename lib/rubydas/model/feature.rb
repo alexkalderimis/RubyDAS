cce1254ecdc13edeaefac3af1e5f5abbca05b4f3
Alex Kalderimis <alex@flymine.org>
2012-02-28 12:48:36 +0000

    Getting model to work

--- lib/rubydas/model/feature.rb
+++ lib/rubydas/model/feature.rb
@@ -6,10 +6,10 @@ class Feature
 
   property :id,         Serial    # An auto-increment integer key
   property :label,      String    # A varchar type string, for short strings
-  property :start,       Number      # A text block, for longer string data.
-  property :end,       Number      # A text block, for longer string data.
+  property :start,       Integer      # A text block, for longer string data.
+  property :stop,       Integer      # A text block, for longer string data.
   property :method, String  # A DateTime, for any date you might like.
-  property :score, Number
+  property :score, Float
   property :phase, Enum[ '0', '1', '2', '-' ]
   property :orientation, Enum[ '0', '-', '+' ]
 
@@ -27,14 +27,12 @@ class Target
   include DataMapper::Resource
 
   property :id, Serial
-  property :start,       Number      # A text block, for longer string data.
-  property :stop,       Number      # A text block, for longer string data.
+  property :start,       Integer      # A text block, for longer string data.
+  property :stop,       Integer      # A text block, for longer string data.
   property :name, String
 
   belongs_to :feature
 end
-
-class
 
 class Note
   include DataMapper::Resource
@@ -42,6 +40,7 @@ class Note
   property :id, Serial
   property :text, String
   belongs_to :feature
+
 end
 
 class Link
@@ -65,4 +64,7 @@ class Type
   has n, :features
   
 end
+ 
+DataMapper.finalize
+
 
