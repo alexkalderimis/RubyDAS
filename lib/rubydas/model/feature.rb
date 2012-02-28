4d0b8f4f8bd1d8c8129a06425149d944e3cea7bb
Alex Kalderimis <alex@flymine.org>
2012-02-28 14:47:45 +0000

    Use end again. Provide public_id in addition to id

--- lib/rubydas/model/feature.rb
+++ lib/rubydas/model/feature.rb
@@ -4,13 +4,14 @@ require 'data_mapper' # requires all the gems listed above
 class Feature
   include DataMapper::Resource
 
-  property :id,         Serial    # An auto-increment integer key
-  property :label,      String    # A varchar type string, for short strings
+  property :id,          Serial    # An auto-increment integer key
+  property :public_id,   String    # An auto-increment integer key
+  property :label,       String    # A varchar type string, for short strings
   property :start,       Integer      # A text block, for longer string data.
-  property :stop,       Integer      # A text block, for longer string data.
-  property :method, String  # A DateTime, for any date you might like.
-  property :score, Float
-  property :phase, Enum[ '0', '1', '2', '-' ]
+  property :end,         Integer      # A text block, for longer string data.
+  property :method,      String  # A DateTime, for any date you might like.
+  property :score,       Float
+  property :phase,       Enum[ '0', '1', '2', '-' ]
   property :orientation, Enum[ '0', '-', '+' ]
 
   has n, :links
